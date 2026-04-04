db6fd544625c492fbf24c9626a715c6c2838c5dc
yoyolasaticot <ydesla01@gmail.com>
2026-04-04 23:31:54 +0200

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,5 +2,13 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  {
+    auth: {
+      persistSession: true,
+      storage: typeof window !== "undefined" ? window.sessionStorage : undefined,
+      autoRefreshToken: true,
+      detectSessionInUrl: true,
+    },
+  }
 );
